d770752f7cdb0cf4661fa914e917e966ba557a2a
Ayush Jain <ayushjain2205@gmail.com>
2024-04-17 12:29:40 +0530

    Add tailwind config and animation css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#FF5705",
+        black: "#262626",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0", transform: "translateY(-20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s ease-out",
       },
     },
   },
