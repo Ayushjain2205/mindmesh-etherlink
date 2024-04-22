68cf2ad0b97541cfa479939e1d23754510713904
Ayush Jain <ayushjain2205@gmail.com>
2024-04-22 16:43:28 +0530

    Bug fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,17 @@ import {
   smartWallet,
 } from "@thirdweb-dev/react";
 import { Toaster } from "react-hot-toast";
-import { MorphTestnet } from "@thirdweb-dev/chains";
+import { EtherlinkTestnet } from "@thirdweb-dev/chains";
 
 const smartWalletConfig = {
-  factoryAddress: "0x77e8DFa0a5A5F50bc4e62A52E8eB4F30a8162176",
+  factoryAddress: "0xe0670FA34Bfd91D1239A52e430D985d3862e132A",
   gasless: true,
 };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      activeChain={MorphTestnet}
+      activeChain={EtherlinkTestnet}
       clientId="aa131b3a51f3247c974b4611ac4ff73e"
       supportedWallets={[smartWallet(embeddedWallet(), smartWalletConfig)]}
     >
